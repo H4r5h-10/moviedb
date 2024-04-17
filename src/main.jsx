473f14838a472ef8605668a3bd63f105d2178079
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2024-04-17 07:12:04 +0530

    Add server

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createContext } from "react";
 
-// export const server = "https://movie-server-6n53.onrender.com";
-export const server = "http://localhost:4000";
+export const server = "https://movie-server-6n53.onrender.com";
+// export const server = "http://localhost:4000";
 
 export const Context = createContext({ isAuthenticated: false });
 
