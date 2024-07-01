da182ecc93f3ceb809380a5df217972bd413d9f7
Harsh <103768218+H4r5h-10@users.noreply.github.com>
2024-07-01 21:45:09 +0530

    Update Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -5,13 +5,14 @@ import { ToastContainer, toast } from "react-toastify";
 import "./signup.css";
 import { Link, Navigate } from "react-router-dom";
 import cookie from "react-cookies";
-// import { HashRouter as Router,  Routes, Route} from 'react-router-dom';
+import Loader from "../loader/Loader.jsx";
 
 const Login = () => {
   const { setIsAuthenticated, isAuthenticated } = useContext(Context);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [cookies, setCookie] = useCookies(['token']);
+  const [loading, setLoading] = useState(false);
+
   const formSubmit = async (e) => {
     e.preventDefault();
     if (email === "" || password === "") {
@@ -22,8 +23,8 @@ const Login = () => {
         theme: "dark",
       });
       return;
-    }
-    const {data} = await axios.post(
+    } else setLoading(true);
+    const { data } = await axios.post(
       `${server}/users/login`,
       {
         email,
@@ -38,14 +39,15 @@ const Login = () => {
     );
     if (data.success) {
       // Set the cookie
-      cookie.save('token', data.token, {
-        path: '/',
+      cookie.save("token", data.token, {
+        path: "/",
         maxAge: 2 * 24 * 60 * 60, // 2 days
         secure: true,
         httpOnly: false, // Should be true if setting the cookie from the server
       });
       setIsAuthenticated(true);
     }
+    if (!isAuthenticated) setLoading(false);
     toast(`${data.message}`, {
       autoClose: 1000,
       closeOnClick: false,
@@ -57,36 +59,47 @@ const Login = () => {
     return <Navigate to={"/watchlist"} />;
   }
   return (
-    <div className="form-container">
-      <form action="post" onSubmit={formSubmit} className="form login-form">
-        <h3>Login Here</h3>
-
-        <label>Username</label>
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <label>Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <div className="button-submit">
-          <button type="submit">Log In</button>
-
-          <Link className="links" to="/signup">
-            <button className="link-btn">Sign Up</button>
-          </Link>
+    <>
+      {loading ? (
+        <div>
+          <Loader/>
+          <div className="transition-signup">
+            <h2>Please wait...</h2>
+          </div>
         </div>
-      </form>
-      <ToastContainer closeButton="false" />
-    </div>
+      ) : (
+        <div className="form-container">
+          <form action="post" onSubmit={formSubmit} className="form login-form">
+            <h3>Login</h3>
+
+            <label>Email</label>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className="button-submit">
+              <button type="submit">Log In</button>
+
+              <Link className="links" to="/signup">
+                <button className="link-btn">Sign Up</button>
+              </Link>
+            </div>
+          </form>
+          <ToastContainer closeButton="false" />
+        </div>
+      )}
+    </>
   );
 };
 
