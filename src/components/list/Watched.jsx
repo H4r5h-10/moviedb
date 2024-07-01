1300989705a1a0c493848f10fd4e71e823d7b300
Harsh <103768218+H4r5h-10@users.noreply.github.com>
2024-07-01 21:42:09 +0530

    Update Watched.jsx

--- src/components/list/Watched.jsx
+++ src/components/list/Watched.jsx
@@ -7,12 +7,13 @@ import { Navigate } from "react-router";
 import { ToastContainer } from "react-toastify";
 import cookie from "react-cookies";
 import Header from "../header/Header.jsx";
+import Loader from "../loader/Loader.jsx";
 
 export const Watched = () => {
+  const [progress, setProgress] = useState(0);
+  const [loading, setLoading] = useState(true);
   const { isAuthenticated, refresh, setRefresh, watched, setWatched } =
     useContext(Context);
-
-  const [progress, setProgress] = useState(0);
   console.log(progress);
   const token = cookie.load("token");
   if (!isAuthenticated) return <Navigate to={"/"} />;
@@ -27,40 +28,41 @@ export const Watched = () => {
       .then((res) => {
         setWatched(res.data.data);
         setRefresh(false);
+        setLoading(false);
       });
   }, [refresh, setWatched, setRefresh]);
 
-  
-
   return (
-   <div>
-    <Header/>
     <div>
-      <LoadingBar progress={100} onLoaderFinished={() => setProgress(0)} />
-      <ToastContainer className={"toasty"} closeButton="false" />
-      <div className="top-container">
-        <h1 className="page-head">
-          <div className="v-line"></div>Watched Movies
-        </h1>
-        <div className="card-container">
-          {watched.length ? (
-            <div className="cards">
-              {watched.map((movie) => (
-                <MovieCard
-                  key={movie.movie.id}
-                  movie={movie.movie}
-                  type="watched"
-                  id={movie.id}
-                />
-              ))}
-            </div>
-          ) : (
-            <h2 className="no">No movies in your Watched List...</h2>
-          )}
+      <Header />
+      <div>
+        <LoadingBar progress={100} onLoaderFinished={() => setProgress(0)} />
+        <ToastContainer className={"toasty"} closeButton="false" />
+        <div className="top-container">
+          <h1 className="page-head">
+            <div className="v-line"></div>Watched Movies
+          </h1>
+          <div className="card-container">
+            {watched.length ? (
+              <div className="cards">
+                {watched.map((movie) => (
+                  <MovieCard
+                    key={movie.movie.id}
+                    movie={movie.movie}
+                    type="watched"
+                    id={movie.id}
+                  />
+                ))}
+              </div>
+            ) : (
+              <div>
+           {loading?<Loader/>:<h2 className="no">No movies in your watched list..</h2>}
+           </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
-   </div>
   );
 };
 
